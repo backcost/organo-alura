57fbafd53368dfe8a0122d6fc0e84132d55fe1c8
Gabriel <gabriel_costa_ferreira@hotmail.com>
2022-10-26 19:11:59 -0300

    how array works in react

--- organo/src/App.jsx
+++ organo/src/App.jsx
@@ -1,12 +1,21 @@
 import Banner from './components/Banner';
 import Formulario from './components/Formulario';
+import { useState } from 'react';
 
 
 function App() {
+  const [colaboradores, setColaboradores] = useState([])
+  const aoColaboradorAdicionado = (colaborador) => {
+    console.log(colaboradores)
+    setColaboradores([...colaboradores, colaborador])
+  }
+
   return (
     <div className="App">
       <Banner />
-      <Formulario />
+      <Formulario 
+        aoColaboradorCadastrado={colaborador => aoColaboradorAdicionado(colaborador)}
+      />
     </div>
   );
 }
--- organo/src/components/ListaSuspensa/index.jsx
+++ organo/src/components/ListaSuspensa/index.jsx
@@ -7,8 +7,9 @@ const ListaSuspensa = (props) => {
             <label>{props.label}</label>
             <select  
                 onChange={evento => props.aoAlterado(evento.target.value)}
-                required={props.obrigatorio}>
-                    {props.itens.map(item => <option key={item}>{item}</option>)}
+                required={props.obrigatorio}
+            >
+                {props.itens.map(item => <option key={item}>{item}</option>)}
             </select>
         </div>
     )
