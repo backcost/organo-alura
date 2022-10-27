78c9a8d0a2d0a6d4be036d6e3946e53d477a75dc
Gabriel <gabriel_costa_ferreira@hotmail.com>
2022-10-26 21:07:26 -0300

    Colaborador

---
+++ organo/src/components/Colaborador/index.jsx
@@ -0,0 +1,17 @@
+import './Colaborador.css'
+
+const Colaborador = () => {
+    return (
+        <div className='colaborador'>
+            <div className='cabecalho'>
+                <img src="https://github.com/backcost.png" alt="Gabriel Costa" />
+            </div>
+            <div className='rodape'>
+                <h4>Gabriel Costa</h4>
+                <h5>FullStack Developer</h5>
+            </div>
+        </div>
+    )
+}
+
+export default Colaborador
--- organo/src/components/Time/index.jsx
+++ organo/src/components/Time/index.jsx
@@ -1,4 +1,5 @@
 import './Time.css'
+import Colaborador from '../Colaborador'
 
 const Time = (props) => {
     const cssSection = { backgroundColor: props.corSecundaria }
@@ -6,6 +7,8 @@ const Time = (props) => {
     return (
         <section className='time' style={cssSection}>
             <h3 style={cssH3}>{props.nome}</h3>
+            <Colaborador />
+            <Colaborador />
         </section>
     )
 }
